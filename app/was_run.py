3f1fad26157970eadb7bfdb310ba75b79bef3a91
leejunkoo <leejunkoo@icloud.com>
2022-08-19 21:41:57 +0900

    ch20

--- app/was_run.py
+++ app/was_run.py
@@ -7,7 +7,11 @@ class WasRun(TestCase):
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
